660e855218c635638e322154de6addce030267c8
mariam <mariam@MYPROBOOK>
2023-02-21 21:40:32 +0400

    Add project files.

---
+++ Classwork21.02part1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Classwork21._02part1
+{
+
+    internal class Program
+    {
+        static int OutMethod(out int t)
+        {
+
+             t = int.Parse(Console.ReadLine());
+            return t*t;
+        }
+        static void OutMethod2(out int a)
+        {
+
+            a = int.Parse(Console.ReadLine());
+            a = a * a;
+            Console.WriteLine(a);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(OutMethod(out int t));
+            OutMethod2(out int a);
+
+        }
+
+    }
+}
